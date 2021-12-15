cc6f6e7270d7b046d05b750c728d3866d81cf507
Matej Mok <matej.mok@gmail.com>
2021-12-15 12:13:58 +0100

    saving and loading redux state from local storage

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import sheetReducer from '../components/Sheet/sheetSlice'
 
+const jsonState = localStorage.getItem('reduxState')
+const preloadedState = jsonState === null ? undefined : JSON.parse(jsonState)
+
 export const store = configureStore({
   reducer: {
     sheet: sheetReducer,
   },
+  preloadedState
 });
+
+store.subscribe(()=>{
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+})
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
