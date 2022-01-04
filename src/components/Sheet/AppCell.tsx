70a295b4f3cb0a767722f757139a03532be95552
Matej Mok <matej.mok@gmail.com>
2022-01-04 18:02:44 +0100

    checking for state change in AppCell with timer

--- src/components/Sheet/AppCell.tsx
+++ src/components/Sheet/AppCell.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { getAppInfo } from '../../EmbeddedApps';
 import { useAppDispatch } from '../../store/hooks';
 import { updateCellData } from './sheetSlice';
@@ -13,34 +13,25 @@ function AppCell(props: AppCellProps) {
   const dispatch = useAppDispatch();
   const { prepare, AppComponent } = getAppInfo(props.type)
 
-  const {instance, getState} = useState(prepare(props.initialState))[0];
+  const {instance, getState} = useRef(prepare(props.initialState)).current;
+  const stateChanged = useRef(false)
 
-  const [ stateChanged, setStateChanged ] = useState(false)
   var onAppStateChanged = () => {
-    console.log(`app of type: ${props.type} on cell ${props.cellId} has changed its state`);
-    console.log('set state to true')
-    setStateChanged(true)
-    console.log('stateChanged ' + stateChanged)
-    //console.log('new state: ');
-    //console.log(getState(instance));
-    dispatch(updateCellData({cellId: props.cellId, data: getState(instance)}));
+    stateChanged.current = true
+    //dispatch(updateCellData({cellId: props.cellId, data: getState(instance)}));
   }
 
   /* check for state change periodically */
-  /*
   useEffect(() => {
     const interval = setInterval(() => {
-      console.log('periodic check ' + stateChanged);
-      if (stateChanged) {
-        console.log('state saved');
+      if (stateChanged.current) {
+        console.log('saving state of ' + props.type + ' in cell ' + props.cellId);
         dispatch(updateCellData({cellId: props.cellId, data: getState(instance)}));
-        setStateChanged(false)
-      } else {
-        console.log('state not saved');
+        stateChanged.current = false
       }
     }, 5000);
     return () => clearInterval(interval);
-  }, []);*/
+  }, []);
 
   return (
     <AppComponent instance={instance} onStateChange={onAppStateChanged}/>
