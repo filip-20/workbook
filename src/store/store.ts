b2bfa7f9bc59c64cde791b41c8ff3d6950913020
Matej Mok <matej.mok@gmail.com>
2022-03-20 15:13:58 +0100

    setupListeners, disabled preloadedState for redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import sheetReducer from './sheetSlice';
 import { githubApi } from '../services/githubApi/endpoints/repos';
 import authReducer from './authSlice';
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
 
 const jsonState = localStorage.getItem('reduxState')
 const preloadedState = jsonState === null ? undefined : {sheet: JSON.parse(jsonState)}
@@ -13,13 +14,18 @@ export const store = configureStore({
     [githubApi.reducerPath]: githubApi.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(githubApi.middleware),
-  preloadedState
+  /*preloadedState*/
 });
 
+// enable listener behavior for the store
+setupListeners(store.dispatch)
+
 store.subscribe(() => {
-  console.log('Saving state as JSON string');
-  localStorage.setItem('reduxState', JSON.stringify(store.getState().sheet));
+  //console.log('Saving state as JSON string');
+  //console.log(store.getState().sheet);
+  //localStorage.setItem('reduxState', JSON.stringify(store.getState().sheet));
 });
+
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
