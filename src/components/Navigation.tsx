270c580004cc448f6e9b3f416f9cefa77b1a7b74
Matej Mok <matej.mok@gmail.com>
2022-04-11 21:25:46 +0200

    show user avatar and login in navbar, updated layout

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,4 @@
-import { Nav, Navbar } from "react-bootstrap";
+import { Container, Image, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { authSelectors } from "../store/authSlice";
 import { useAppSelector } from "../store/hooks";
@@ -6,20 +6,25 @@ import { getLoginUrl } from "./LoginPage";
 
 function Navigation() {
   const accessToken = useAppSelector(authSelectors.accessToken);
+  const user = useAppSelector(authSelectors.user);
   return (
     <Navbar expand="sm" bg="dark" variant="dark">
-      <Navbar.Brand className="mx-3">Workbook</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse>
-        <Nav className="ms-auto me-3">
-        <Nav.Link as={Link} to="/repos">Zoznam repozitárov</Nav.Link>
-          {
-            accessToken ?
-              <Nav.Link as={Link} to="/logout">Odhlásiť sa</Nav.Link>
-              : <Nav.Link href={getLoginUrl()}>Prihlásiť sa</Nav.Link>
-          }
-        </Nav>
-      </Navbar.Collapse>
+        <Navbar.Brand className="ms-3">Workbook</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav className="ms-auto" style={{marginRight: '4.5rem'}}>
+            <Nav.Link as={Link} to="/repos">Zoznam repozitárov</Nav.Link>
+            {
+              accessToken ?
+                (
+                  <NavDropdown menuVariant="dark" title={<><Image roundedCircle src={user?.avatarUrl} className="me-2" style={{ height: '1.5rem' }} />{user?.login}</>}>
+                    <NavDropdown.Item ><Nav.Link as={Link} to="/logout">Odhlásiť sa</Nav.Link></NavDropdown.Item>
+                  </NavDropdown>
+                )
+                : <Nav.Link href={getLoginUrl()}>Prihlásiť sa</Nav.Link>
+            }
+          </Nav>
+        </Navbar.Collapse>
     </Navbar>
   )
 }
