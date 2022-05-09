8d213afb48f56837bbcef974099a6d985e98004d
Matej Mok <matej.mok@gmail.com>
2022-05-09 18:22:12 +0200

    LogoutPage: redirect to home page after logout

--- src/components/LogoutPage.tsx
+++ src/components/LogoutPage.tsx
@@ -1,14 +1,17 @@
 import { useEffect } from "react";
 import { Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import { logout } from "../store/authSlice";
 import { useAppDispatch } from "../store/hooks";
 
 function LogoutPage() {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     console.log('logout');
     dispatch(logout())
+    navigate('/');
   }, [])
 
   return (
