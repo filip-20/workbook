531a708dc4d8f82e05cf0f3fb1e046e5820d4a76
Matej Mok <matej.mok@gmail.com>
2022-05-09 18:23:16 +0200

    Navigation: updated navigation layout

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,28 +3,33 @@ import { Link } from "react-router-dom";
 import { authSelectors } from "../store/authSlice";
 import { useAppSelector } from "../store/hooks";
 import { getLoginUrl } from "./LoginPage";
+import { PopoverNavLink } from "./PopoverMenu";
+
+import config from '../config.json';
 
 function Navigation() {
   const accessToken = useAppSelector(authSelectors.accessToken);
   const user = useAppSelector(authSelectors.user);
   return (
     <Navbar expand="sm" bg="dark" variant="dark">
-        <Navbar.Brand className="ms-3">Workbook</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse>
-          <Nav className="ms-auto" style={{marginRight: '4.5rem'}}>
-            <Nav.Link as={Link} to="/repos">Zoznam repozitárov</Nav.Link>
-            {
-              accessToken ?
-                (
-                  <NavDropdown menuVariant="dark" title={<><Image roundedCircle src={user?.avatarUrl} className="me-2" style={{ height: '1.5rem' }} />{user?.login}</>}>
-                    <NavDropdown.Item ><Nav.Link as={Link} to="/logout">Odhlásiť sa</Nav.Link></NavDropdown.Item>
-                  </NavDropdown>
-                )
-                : <Nav.Link href={getLoginUrl()}>Prihlásiť sa</Nav.Link>
-            }
-          </Nav>
-        </Navbar.Collapse>
+      <Navbar.Brand href={config.frontend.basePath} className="ms-3">Workbook</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse>
+        <Nav>
+          <Nav.Link as={Link} to="/repos">Zoznam repozitárov</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+      <Nav className="me-3">
+        {
+          accessToken ?
+            (
+              <PopoverNavLink title={<><Image roundedCircle src={user?.avatarUrl} className="me-2" style={{ height: '1.5rem' }} />{user?.login}</>}>
+                <Nav.Link as={Link} to="/logout">Odhlásiť sa</Nav.Link>
+              </PopoverNavLink>
+            )
+            : <Nav.Link href={getLoginUrl()}>Prihlásiť sa</Nav.Link>
+        }
+      </Nav>
     </Navbar>
   )
 }
