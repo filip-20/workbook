acc2deff066d937d9dbeb84afbce0ee81db474b0
Matej Mok <matej.mok@gmail.com>
2022-08-28 11:40:51 +0200

    Updated style of FormattedTextRenderer

--- src/components/FormattedTextRenderer.tsx
+++ src/components/FormattedTextRenderer.tsx
@@ -1,11 +1,12 @@
-import 'katex/dist/katex.min.css';
-
 import ReactMarkdown from "react-markdown";
 import rehypeKatex from "rehype-katex";
 import rehypeRaw from "rehype-raw";
 import rehypeSanitize, { defaultSchema } from 'rehype-sanitize'
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
+
+import 'katex/dist/katex.min.css';
+import styles from './FormattedTextRenderer.module.css'
 
 export interface FormattedTextRendererProps {
   className?: string,
@@ -32,7 +33,7 @@ export default function FormattedTextRenderer(props: FormattedTextRendererProps)
 
   return (
     <ReactMarkdown
-      className={className}
+      className={`${className} ${styles.formattedText}`}
       children={text}
       remarkPlugins={[remarkMath, remarkGfm]}
       rehypePlugins={[rehypeRaw, [rehypeSanitize, rehypeSanitizeOptions], rehypeKatex]}
