320f5da5ac593ae5babe3246ff2d3f2bf418a750
filip-20 <filip20@uniba.sk>
2025-06-10 17:43:55 +0200

    added new structure explorer cell and bumped bootstrap version

--- src/embeddedApps.ts
+++ src/embeddedApps.ts
@@ -1,67 +1,76 @@
-import ResolutionEditor from '@fmfi-uk-1-ain-412/resolution-editor'
-import StructureExplorer from '@fmfi-uk-1-ain-412/fol-graphexplorer'
-import TableauEditor from '@fmfi-uk-1-ain-412/tableaueditor'
-import FormalizationCheckerConf from '@fmfi-uk-1-ain-412/formalization-checker'
-import { CellContext } from './features/sheet/slice/logicContext'
+import ResolutionEditor from "@fmfi-uk-1-ain-412/resolution-editor";
+import StructureExplorer from "@fmfi-uk-1-ain-412/fol-graphexplorer";
+import TableauEditor from "@fmfi-uk-1-ain-412/tableaueditor";
+import FormalizationCheckerConf from "@fmfi-uk-1-ain-412/formalization-checker";
+import { CellContext } from "./features/sheet/slice/logicContext";
+
+import NewStructureExplorer from "@fmfi-uk-1-ain-412/structure-explorer";
 
 export interface PrepareResult {
-  instance: any,
-  getState: (instance: any) => any,
+  instance: any;
+  getState: (instance: any) => any;
 }
 type PrepareFunction = (
   initialState: any,
-  additionalArgs?: any,
-) => PrepareResult
+  additionalArgs?: any
+) => PrepareResult;
 
 interface AppComponentProps {
-  instance: any,
-  onStateChange: () => void,
-  isEdited: boolean,
-  context?: CellContext,
-  proof?: any,
-  updateProofVerdict?: (verdict: boolean) => void,
-} 
-
-interface EmeddedApp {
-  name: string,
-  typeName: string,
-  supportsProofs: boolean,
-  prepare: PrepareFunction,
-  AppComponent: (props: AppComponentProps) => JSX.Element,
+  instance: any;
+  onStateChange: () => void;
+  isEdited: boolean;
+  context?: CellContext;
+  proof?: any;
+  updateProofVerdict?: (verdict: boolean) => void;
 }
 
-export const embeddedApps: EmeddedApp[] = 
-  [
-    {
-      name: 'Structure explorer',
-      typeName: 'structureExplorer',
-      supportsProofs: false,
-      ...StructureExplorer
-    },
-    {
-      name: 'Tableau editor',
-      typeName: 'tableauEditor',
-      supportsProofs: true,
-      ...TableauEditor
-    },
-    {
-      name: 'Resolution editor',
-      supportsProofs: true,
-      typeName: 'resolutionEditor',
-      ...ResolutionEditor
-    },
-    {
-      name: 'Formalization checker',
-      supportsProofs: false,
-      typeName: 'formalizationChecker',
-      ...FormalizationCheckerConf('https://student.dai.fmph.uniba.sk/services/formalization-checker')
-    }
-  ]
+interface EmeddedApp {
+  name: string;
+  typeName: string;
+  supportsProofs: boolean;
+  prepare: PrepareFunction;
+  AppComponent: (props: AppComponentProps) => JSX.Element;
+}
 
-var appType2AppInfo: { [key: string]: EmeddedApp} = {};
+export const embeddedApps: EmeddedApp[] = [
+  {
+    name: "Structure explorer",
+    typeName: "structureExplorer",
+    supportsProofs: false,
+    ...StructureExplorer,
+  },
+  {
+    name: "Tableau editor",
+    typeName: "tableauEditor",
+    supportsProofs: true,
+    ...TableauEditor,
+  },
+  {
+    name: "Resolution editor",
+    supportsProofs: true,
+    typeName: "resolutionEditor",
+    ...ResolutionEditor,
+  },
+  {
+    name: "Formalization checker",
+    supportsProofs: false,
+    typeName: "formalizationChecker",
+    ...FormalizationCheckerConf(
+      "https://student.dai.fmph.uniba.sk/services/formalization-checker"
+    ),
+  },
+  {
+    name: "New Structure explorer",
+    typeName: "newStructureExplorer",
+    supportsProofs: false,
+    ...NewStructureExplorer,
+  },
+];
 
-embeddedApps.forEach( app => {
-  appType2AppInfo[app.typeName] = app
+var appType2AppInfo: { [key: string]: EmeddedApp } = {};
+
+embeddedApps.forEach((app) => {
+  appType2AppInfo[app.typeName] = app;
 });
 
 export function getAppInfo(typeName: string): EmeddedApp {
